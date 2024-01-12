13e5684232229914ecb09680d7748e58603a6156
Marques Johnson <89541657+MarqJ21@users.noreply.github.com>
2024-01-12 14:03:15 -0500

    Add files via upload

---
+++ DojoWeather/static/script.js
@@ -0,0 +1,20 @@
+function accept(){
+    console.log("clicked")
+    var stop = document.querySelector("#stop");
+    stop.remove();
+}
+
+function city(element){
+    alert("Loading weather report");
+}
+var todaytemp = document.querySelector("#today_low")
+//Wasn't able to figure out
+function scale(){
+    
+    todaytemp.innerHTML = "40";
+    var todaytemp = document.querySelector(".low")
+    todaytemp.innerHTML = "27";
+
+}
+
+scale();
